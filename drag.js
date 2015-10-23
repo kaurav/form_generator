da516bcfcbc6d16381712587199ce62a81f37838
Avi_kaur <avikashyap620@yahoo.com>
2015-10-24 00:12:53 +0530

    First

---
+++ drag.js
@@ -0,0 +1,6 @@
+         function dragStart(ev) {
+            ev.dataTransfer.effectAllowed='move';
+            ev.dataTransfer.setData("Text", ev.target.getAttribute('id'));
+            ev.dataTransfer.setDragImage(ev.target,0,0);
+            
+            return true;
